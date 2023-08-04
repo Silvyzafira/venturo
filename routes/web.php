ab87db35b41bee88ddd6bc42b9c6ea624c174e24
SilvyZafira <zafirasilvy@gmail.com>
2023-08-04 13:13:39 +0700

    update route

--- routes/web.php
+++ routes/web.php
@@ -17,8 +17,8 @@ use App\Http\Controllers\VenturoController;
 // Route::get('/intermediate', function () {return view('welcome');});
 Route::get('/intermediate/menu', [VenturoController::class, 'getMenuFromAPI']);
 Route::get('/intermediate/tahun', [VenturoController::class, 'getTransaksiByTahun'])->name('getTransaksiByTahun');
-Route::get('/intermediate', [VenturoController::class, 'getMenu']);
+// Route::get('/intermediate', [VenturoController::class, 'getMenu']);
 Route::get('/test', [VenturoController::class, 'index']);
-Route::get('/fetch-data', [VenturoController::class, 'fetchData'])->name('fetch_data');
+Route::get('/intermediate', [VenturoController::class, 'fetchData'])->name('fetch_data');
 
 
